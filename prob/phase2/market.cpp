b93af1abc68568787b4a56ee787ee55d31be5ae5
manavpatel03 <manavpatel871@gmail.com>
2023-11-29 00:08:42 +0530

    final

--- prob/phase2/market.cpp
+++ prob/phase2/market.cpp
@@ -8,6 +8,15 @@
 
 using namespace std;
 
+bool srch(vector<string> &a, string b)
+{
+    for (int i = 0; i < a.size(); i++)
+    {
+        if (a[i] == b)
+            return true;
+    }
+    return false;
+}
 int min(int &a, int &b)
 {
     if (a <= b)
@@ -44,6 +53,13 @@ void market::start()
         }
         orders.push_back(line);
     }
+    hashMap *qunttrade_b = new hashMap();
+    hashMap *qunttrade_s = new hashMap();
+    hashMap *net = new hashMap();
+    int completed_trades = 0;
+    int shares_traded = 0;
+    int moni = 0;
+    vector<string> broker_s;
     for (string order : orders)
     {
 
@@ -86,6 +102,8 @@ void market::start()
         {
             stocks2.push_back(stoi(stocks[i]));
         }
+        if (!srch(broker_s, Broker))
+            broker_s.push_back(Broker);
         // cout << stocks[stocks.size() - 1];
         //  if (stocks.size() % 2 != 0)
         //  {
@@ -122,11 +140,19 @@ void market::start()
             {
                 if (S->quant == min(S->quant, Dealholder[i]->quant))
                 {
+                    completed_trades++;
                     // if(Dealholder[i]->Broke != S->Broke) continue;
                     Dealholder[i]->quant -= S->quant;
-                    S->quant = 0;
+                    shares_traded += S->quant;
+                    moni += S->quant * Dealholder[i]->price;
                     if (S->buy)
                     {
+                        qunttrade_b->addorinsert(S->Broke, S->quant);
+                        qunttrade_s->addorinsert(Dealholder[i]->Broke, S->quant);
+                        net->addorinsert(S->Broke, (-1) * S->quant * Dealholder[i]->price);
+                        net->addorinsert(Dealholder[i]->Broke, S->quant * Dealholder[i]->price);
+                        S->quant = 0;
+
                         cout << S->Broke << " purchased " << li_co_qu << " share of ";
                         if (stocks1.size() == 1)
                         {
@@ -143,6 +169,12 @@ void market::start()
                     }
                     else
                     {
+                        qunttrade_s->addorinsert(S->Broke, S->quant);
+                        qunttrade_b->addorinsert(Dealholder[i]->Broke, S->quant);
+                        net->addorinsert(S->Broke, S->quant * Dealholder[i]->price);
+                        net->addorinsert(Dealholder[i]->Broke, -1 * S->quant * Dealholder[i]->price);
+                        S->quant = 0;
+
                         cout << Dealholder[i]->Broke << " purchased " << li_co_qu << " share of ";
                         if (stocks1.size() == 1)
                         {
@@ -160,10 +192,20 @@ void market::start()
                 }
                 else
                 {
-                    Dealholder[i]->valid = 0;
+                    shares_traded += Dealholder[i]->quant;
+
+                    moni += Dealholder[i]->quant * Dealholder[i]->price;
+                    completed_trades++;
+                    Dealholder[i]
+                        ->valid = 0;
                     S->quant -= Dealholder[i]->quant;
                     if (S->buy)
                     {
+                        qunttrade_b->addorinsert(S->Broke, Dealholder[i]->quant);
+                        qunttrade_s->addorinsert(Dealholder[i]->Broke, Dealholder[i]->quant);
+
+                        net->addorinsert(S->Broke, (-1) * Dealholder[i]->quant * Dealholder[i]->price);
+                        net->addorinsert(Dealholder[i]->Broke, Dealholder[i]->quant * Dealholder[i]->price);
                         cout << S->Broke << " purchased " << Dealholder[i]->quant << " share of ";
                         if (stocks1.size() == 1)
                         {
@@ -180,6 +222,10 @@ void market::start()
                     }
                     else
                     {
+                        qunttrade_s->addorinsert(S->Broke, Dealholder[i]->quant);
+                        qunttrade_b->addorinsert(Dealholder[i]->Broke, Dealholder[i]->quant);
+                        net->addorinsert(S->Broke, Dealholder[i]->quant * Dealholder[i]->price);
+                        net->addorinsert(Dealholder[i]->Broke, -1 * Dealholder[i]->quant * Dealholder[i]->price);
                         cout << Dealholder[i]->Broke << " purchased " << Dealholder[i]->quant << " share of ";
                         if (stocks1.size() == 1)
                         {
@@ -201,5 +247,13 @@ void market::start()
                 myorders.insert(S);
             }
         }
+    }
+    cout << "---End of Day---" << endl;
+    cout << "Total Amount of Money Transferred: $" << moni << endl;
+    cout << "Number of Completed Trades: " << completed_trades << endl;
+    cout << "Number of Shares Traded: " << shares_traded << endl;
+    for (int i = 0; i < broker_s.size(); i++)
+    {
+        cout << broker_s[i] << " bought " << qunttrade_b->search_Val(broker_s[i]) << "and sold " << qunttrade_s->search_Val(broker_s[i]) << "for a net transfer of $" << net->search_Val(broker_s[i]) << endl;
     }
 }
